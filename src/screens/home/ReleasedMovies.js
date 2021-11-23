be685558c44d31d6d82756864618482b6d294ded
Nitesh Singh <nitesh@mollatech.com>
2021-11-23 23:13:36 +0530

    Basic filter added

--- src/screens/home/ReleasedMovies.js
+++ src/screens/home/ReleasedMovies.js
@@ -1,8 +1,22 @@
-import React from "react"
-import "./ReleasedMovies.css"
+import React from "react";
+import "./ReleasedMovies.css";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import CardHeader from "@mui/material/CardHeader";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { TextField } from "@material-ui/core";
+import MenuItem from "@mui/material/MenuItem";
+import Checkbox from "@mui/material/Checkbox";
+import ListItemText from "@mui/material/ListItemText";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
 const itemData = [
   {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
@@ -76,32 +90,177 @@ const itemData = [
     cols: 2,
   },
 ];
-const ReleasedMovies = () => {
-    return (
-      <div className="released__main__container">
-        <div className="released__images__grid">
-          <ImageList sx={{ width: "100%", height: 350 }} cols={4}>
-            
-            {itemData.map((item) => (
-              <ImageListItem key={item.img}>
-                <img
-                  src={`${item.img}?w=248&fit=crop&auto=format`}
-                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  alt={item.title}
-                  loading="lazy"
-                />
-                <ImageListItemBar
-                  title={item.title}
-                  subtitle={item.author}
-                 
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </div>
-        <div className="released__filters_container"></div>
-      </div>
-    );
-}
+const names = [
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
+];
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
 
-export default ReleasedMovies;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+const styles = (theme) => ({
+  card: {
+    maxWidth: 240,
+    minWidth: 240,
+    margin: theme.spacing.unit,
+  },
+  heading: {
+    color: theme.palette.primary.light,
+    marginLeft:20
+  },
+    fields: {
+      marginBottom: theme.spacing.unit * 2,
+  }
+});
+const ReleasedMovies = (props) => {
+    
+    const { classes } = props;
+    const [personName, setPersonName] = React.useState([]);
+    const handleChange = (event) => {
+      const {
+        target: { value },
+      } = event;
+      setPersonName(
+        // On autofill we get a the stringified value.
+        typeof value === "string" ? value.split(",") : value
+      );
+    };
+  return (
+    <div className="released__main__container">
+      <div className="released__images__grid">
+        <ImageList sx={{ width: "100%", height: 350 }} cols={4}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar title={item.title} subtitle={item.author} />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </div>
+      <div className="released__filters_container">
+        <Card className={classes.card}>
+          {/* <CardHeader className={classes.heading} title="FIND MOVIES BY:" /> */}
+          <p className={classes.heading}>FIND MOVIES BY:</p>
+
+          <CardContent>
+            <form>
+              <div className={classes.fields}>
+                <TextField label="Movie Name" variant="standard" size="small" />
+              </div>
+
+              <div className={classes.fields}>
+                <FormControl sx={{ width: "90%" }}>
+                  <InputLabel id="demo-multiple-checkbox-label">
+                    Genere
+                  </InputLabel>
+                  <Select
+                    labelId="demo-multiple-checkbox-label"
+                    id="demo-multiple-checkbox"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<Input label="Genere" size="small" />}
+                    renderValue={(selected) => selected.join(", ")}
+                    MenuProps={MenuProps}
+                  >
+                    {names.map((name) => (
+                      <MenuItem key={name} value={name}>
+                        <Checkbox checked={personName.indexOf(name) > -1} />
+                        <ListItemText primary={name} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </div>
+              <div className={classes.fields}>
+                <FormControl sx={{ width: "90%" }}>
+                  <InputLabel id="demo-multiple-checkbox-label">
+                    Artists
+                  </InputLabel>
+                  <Select
+                    labelId="demo-multiple-checkbox-label"
+                    id="demo-multiple-checkbox"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<Input label="Artists" size="small" />}
+                    renderValue={(selected) => selected.join(", ")}
+                    MenuProps={MenuProps}
+                  >
+                    {names.map((name) => (
+                      <MenuItem key={name} value={name}>
+                        <Checkbox checked={personName.indexOf(name) > -1} />
+                        <ListItemText primary={name} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </div>
+              <div className={classes.fields}>
+                <TextField
+                  size="small"
+                  label="Release Date Start"
+                  variant="standard"
+                  type="date"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </div>
+              <div className={classes.fields}>
+                <TextField
+                  size="small"
+                  label="Release Date End"
+                  variant="standard"
+                  type="date"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </div>
+
+              <div className={classes.fields}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  size="small"
+                >
+                  Apply
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+
+
+ReleasedMovies.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ReleasedMovies);
